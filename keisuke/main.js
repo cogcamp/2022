b58346dacfaee79400c864f9df2a9f3de87e2d93
cogcamp <codeofgenius.camp@gmail.com>
2022-03-27 06:49:22 +0000

    keisuke

--- keisuke/main.js
+++ keisuke/main.js
@@ -2,16 +2,23 @@ var mainScene = new Phaser.Scene("mainScene");
 
 mainScene.create = function() {
     // 初期設定を実行する
-    
+    this.config();
     
     // ボール作成
-    
+    this.createBall();
     
     // パドル作成
-    
+    this.createPaddle();
     
     // スペースキーのクリックでボール発射
-    
+    this.input.keyboard.on('keydown-SPACE',function(event){
+        //ゲーム開始状態ならば
+        if(this.paddle.isStart){
+            //ボール発射
+        　　this.ball.setVelocity(this.ballSpeedX,this.ballSpeedY);
+        　　this.paddle.isStart=false;
+        }
+    },this);
     
     // ブロック作成
     
@@ -27,7 +34,20 @@ mainScene.update = function() {
     // キーボードのカーソルオブジェクトを取得
     var cursors = this.input.keyboard.createCursorKeys();
     var x = 0;
-    
+//右カーソルをクリックすると
+if(cursors.right.isDown){
+    x = this.paddle.x + this.paddleSpeed;
+    this.paddle.x = Phaser.Math.Clamp(x,52,748);
+}
+//左カーソルをクリックすると
+if(cursors.left.isDown){
+    x = this.paddle.x - this.paddleSpeed;
+    this.paddle.x= Phaser.Math.Clamp(x,52,748);
+}
+//パドルの上にボールが乗っているなら
+if(this.paddle.isStart){
+    this.ball.setPosition(this.paddle.x,500);
+}
 };
 
 mainScene.config = function() {
@@ -47,18 +67,38 @@ mainScene.config = function() {
 
 mainScene.createBall = function() {
     // ボール作成
-    
+    this.ball=this.physics.add.image(400,500,'ball1');
+    this.ball.setDisplaySize(22,22);
+    this.ball.setCollideWorldBounds(true);
+    this.ball.setBounce(1);
 };
 
 mainScene.createPaddle = function() {
      // パドル作成
-    
-};
+    this.paddle=this.physics.add.image(400,550,'paddle1');
+    this.paddle.setDisplaySize(104,24);
+    this.paddle.setImmovable();
+    this.paddle.isStart=true;
+    this.physics.add.collider(this.paddle,this.ball,this.hitPaddle,null,this);
+}
 
 mainScene.hitPaddle = function (paddle, ball) {
     // ボールにX方向の角度を設定
-    
-};
+    var diff =0;
+    if(ball.x < paddle.x){
+    //ボールがパドルの左側に衝突
+    diff=paddle.x-ball.x;
+    ball.setVelocityX(10 * diff);
+}   else if (ball.x>paddle.x){
+    //ボールがパドルの右側に衝突
+    biff = ball.x -paddle.x;
+    ball.setVelocityX(10 * diff);
+}else{
+    //x方向の加速度はなし
+    ball.setVelocityX(0);
+}
+};    
+
 
 mainScene.createBlocks = function() {
     // 横10列、縦6行並べる
